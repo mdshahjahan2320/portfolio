c0ee2cb0aac850721ed3d700e3543e3b8ccdc9d9
Md Shahjahan <143617101+mdshahjahan2320@users.noreply.github.com>
2026-06-13 16:23:22 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
-console.log('portfolio');
+console.log("Portfolio Loaded");
+
+document.querySelectorAll('a[href^="#"]')
+.forEach(anchor => {
+
+anchor.addEventListener("click", function(e){
+
+e.preventDefault();
+
+document.querySelector(
+this.getAttribute("href")
+).scrollIntoView({
+
+behavior:"smooth"
+
+});
+
+});
+
+});
